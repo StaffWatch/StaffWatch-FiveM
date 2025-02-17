b699f875eaa676f6842b206800c362625aa92779
Aiden <43355457+AidenTheTechBoy@users.noreply.github.com>
2025-02-16 22:11:37 -0500

    Only make update request if players are online

--- server/recurringUpdate.lua
+++ server/recurringUpdate.lua
@@ -3,53 +3,60 @@ local UPDATE_INTERVAL = 5000
 Citizen.CreateThread(function()
   while true do
     
+    -- Initialize players and dto
+    local players = GetPlayers()
     local playersDto = {}
 
-    -- Add each player to dto
-    local players = GetPlayers()
-    for _, playerId in ipairs(players) do
-        local playerDto = CreatePlayerDTO(nil, playerId)
-        table.insert(playersDto, playerDto)
-    end
+    -- Skip if no online players
+    if (#players > 0) then
 
-    -- Convert request to JSON
-    local jsonData = json.encode({
-        secret = Config.SECRET,
-        players = playersDto
-    })
+      -- Add each player to dto
+      for _, playerId in ipairs(players) do
+          local playerDto = CreatePlayerDTO(nil, playerId)
+          table.insert(playersDto, playerDto)
+      end
 
-    DebugLog('Scheduled update JSON:')
-    DebugLog(jsonData)
+      -- Convert request to JSON
+      local jsonData = json.encode({
+          secret = Config.SECRET,
+          players = playersDto
+      })
 
-    -- Send request to server
-    local status, resultData, resultHeaders, errorData = PerformHttpRequestAwait(
-        Config.API_URL .. "/api/update", "POST", jsonData, {["Content-Type"] = 'application/json'}
-    )
-    
-    -- Handle failure
-    if (status ~= 200) then
-        print("Warning: Scheduled update failed with status code: " .. status)
-        print("Failure reason: " .. errorData)
-    end
+      DebugLog('Scheduled update JSON:')
+      DebugLog(jsonData)
 
-    -- Get result from API call
-    DebugLog(resultData)
-    local result = json.decode(resultData)
-    
-    -- Handle null result
-    if (result == nil) then
-      print("Warning: Response was null")
-      return
-    end
+      -- Send request to server
+      local status, resultData, resultHeaders, errorData = PerformHttpRequestAwait(
+          Config.API_URL .. "/api/update", "POST", jsonData, {["Content-Type"] = 'application/json'}
+      )
+      
+      -- Handle failure
+      if (status ~= 200) then
+          print("Warning: Scheduled update failed with status code: " .. status)
+          print("Failure reason: " .. errorData)
+      end
 
-    -- Execute commands from command queue
-    local commands = result.commands
-    for i in pairs(commands) do
-      DebugLog("Execute command: ".. commands[i])
-      ExecuteCommand(commands[i])
+      -- Get result from API call
+      DebugLog(resultData)
+      local result = json.decode(resultData)
+      
+      -- Handle null result
+      if (result == nil) then
+        print("Warning: Response was null")
+        return
+      end
+
+      -- Execute commands from command queue
+      local commands = result.commands
+      for i in pairs(commands) do
+        DebugLog("Execute command: ".. commands[i])
+        ExecuteCommand(commands[i])
+      end
+
     end
 
     -- Wait before next update
     Citizen.Wait(UPDATE_INTERVAL)
+    
   end
 end)
