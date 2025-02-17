9c1c382dbd18f48d9955de8c72455e4dfde9d52a
Aiden <43355457+AidenTheTechBoy@users.noreply.github.com>
2025-02-17 13:27:21 -0500

    Register templated actions on startup

--- config.lua
+++ config.lua
@@ -8,6 +8,20 @@ Config = {
     SECRET = "ubxPHQwNJPlPiZkXhPou",
 
     -- Allow join when connection fails?
-    BYPASS_ON_FAILURE = true
+    BYPASS_ON_FAILURE = true,
+
+    -- Configure templated actions
+    TEMPLATED_ACTIONS = {
+        {
+            playerLevel = true,
+            name = "Freeze",
+            command = "sw_freeze"
+        },
+        {
+            playerLevel = true,
+            name = "Unfreeze",
+            command = "sw_unfreeze"
+        },
+    }
 
 }
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,7 @@ description 'FiveM integration for the StaffWatch web panel'
 version '1.0.0'
 server_scripts {
     "config.lua",
+    "/src/server/register.lua",
     "/src/server/util.lua",
     "/src/server/playerJoin.lua",
     "/src/server/recurringUpdate.lua",
