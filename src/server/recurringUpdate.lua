72266ce7be506edb499d1a9261034c878e0a8bcf
Aiden <43355457+AidenTheTechBoy@users.noreply.github.com>
2025-12-05 19:19:04 -0500

    Send update even if there are no players online (to prevent server from showing offline)

--- src/server/recurringUpdate.lua
+++ src/server/recurringUpdate.lua
@@ -12,41 +12,36 @@ end)
 
 function DoRecurringUpdate()
   -- Initialize players and dto
-    local players = GetPlayers()
-    local playersDto = {}
+  local players = GetPlayers()
+  local playersDto = {}
 
-    -- Skip if no online players
-    if (#players > 0) then
+  -- Add each player to dto
+  for _, playerId in ipairs(players) do
+      local playerDto = CreatePlayerDTO(nil, playerId)
+      table.insert(playersDto, playerDto)
+  end
 
-      -- Add each player to dto
-      for _, playerId in ipairs(players) do
-          local playerDto = CreatePlayerDTO(nil, playerId)
-          table.insert(playersDto, playerDto)
-      end
+  -- Convert request to JSON
+  local jsonData = json.encode({
+      secret = Config.SECRET,
+      players = playersDto
+  })
 
-      -- Convert request to JSON
-      local jsonData = json.encode({
-          secret = Config.SECRET,
-          players = playersDto
-      })
+  DebugLog('Scheduled update JSON:')
+  DebugLog(jsonData)
 
-      DebugLog('Scheduled update JSON:')
-      DebugLog(jsonData)
+  -- Send request to server
+  local status, resultData, resultHeaders, errorData = PerformHttpRequestAwait(
+      Config.API_URL .. "/api/check-in", "POST", jsonData, {["Content-Type"] = 'application/json'}
+  )
+  
+  -- Handle failure
+  if (status ~= 200) then
+      print("Warning: Scheduled update failed with status code: " .. status)
+      print("Failure reason: " .. errorData)
+      return
+  end
 
-      -- Send request to server
-      local status, resultData, resultHeaders, errorData = PerformHttpRequestAwait(
-          Config.API_URL .. "/api/check-in", "POST", jsonData, {["Content-Type"] = 'application/json'}
-      )
-      
-      -- Handle failure
-      if (status ~= 200) then
-          print("Warning: Scheduled update failed with status code: " .. status)
-          print("Failure reason: " .. errorData)
-          return
-      end
-
-      -- Get result from API call
-      DebugLog(resultData)
-
-    end
+  -- Get result from API call
+  DebugLog(resultData)
 end
